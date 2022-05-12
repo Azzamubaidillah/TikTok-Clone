1ff435800e1cd0bf74d90b7d5c3f07ac89f05e5e
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-05-12 10:29:27 +0700

    upload image and get thumbnail

---
+++ lib/controllers/upload_video_controller.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok/constants.dart';
+import 'package:video_compress/video_compress.dart';
+
+class UploadVideoController extends GetxController {
+  _compressVideo(String videoPath) async {
+    final compressedVideo = await VideoCompress.compressVideo(
+      videoPath,
+      quality: VideoQuality.MediumQuality,
+    );
+    return compressedVideo!.file;
+  }
+
+  Future<String> _uploadVideoToStorage(String id, String videoPath) async {
+    Reference ref = firebaseStorage.ref().child('videos').child(id);
+
+    UploadTask uploadTask = ref.putFile(await _compressVideo(videoPath));
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  _getThumbnail(String videoPath) async {
+    final thumbnail = await VideoCompress.getFileThumbnail(videoPath);
+    return thumbnail;
+  }
+
+  Future<String> _uploadImageToStorage(String id, String videoPath) async {
+    Reference ref = firebaseStorage.ref().child('thumbnails').child(id);
+    UploadTask uploadTask = ref.putFile(await _getThumbnail(videoPath));
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  //upload video
+  uploadVideo(String songName, String caption, String videoPath) async {
+    try {
+      String uid = firebaseAuth.currentUser!.uid;
+      DocumentSnapshot userDoc =
+          await firestore.collection('users').doc(uid).get();
+      var allDocs = await firestore.collection('videos').get();
+      int len = allDocs.docs.length;
+
+      String vidoeUrl = await _uploadVideoToStorage("Video $len", videoPath);
+      _uploadImageToStorage("Video $len", videoPath);
+    } catch (e) {}
+  }
+}
